9e7d0ffb5ab4e202aff46eec651c209107420864
Patrick Smith <smithpm@usc.edu>
2024-06-06 16:24:54 -0700

    refactor to use FakeLED

--- test/LEDDriver/test_LEDController.c
+++ test/LEDDriver/test_LEDController.c
@@ -1,16 +1,30 @@
 #include "unity.h"
-#include "LED.h"
 #include "LEDController.h"
 #include "FakeSW1.h"
-#include "IO.h"
+// #include "IO.h"
+#include "FakeLED.h"
 
 void testLEDOffWhenSwitchOpen(void) {
     LEDController_Create();
     FakeSW1_Create();
+    FakeLED_Create();
+    
     FakeSW1_Open();
-
     LED_Controller_UpdateLED();
+    
     TEST_ASSERT_EQUAL_INT32(LED_OFF, LED_GetState());
+    
     LEDController_Destroy();
     FakeSW1_Destroy();
+    FakeLED_Destroy();
 }
+
+// void testLEDOnWhenSwitchClosed(void) {
+//     LEDController_Create();
+//     FakeSW1_Create();
+//     FakeSW1_Close();    
+//     LED_Controller_UpdateLED();
+//     TEST_ASSERT_EQUAL_INT32(LED_ON, LED_GetState());
+//     LEDController_Destroy();
+//     FakeSW1_Destroy();
+// }

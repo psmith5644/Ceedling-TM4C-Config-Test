dd8deb495f3d9b06ea0ab8f767fcccc6dfc3eee1
Patrick Smith <smithpm@usc.edu>
2024-06-06 14:00:55 -0700

    clean LED files from previous project to prepare for test-driving

--- include/LED.h
+++ include/LED.h
@@ -1,14 +1,5 @@
 #ifndef __LED_H__
 #define __LED_H__
 
-typedef enum {
-    LED_OFF = 0, LED_ON
-} LED_STATE;
-
-void LED_Create(void);
-void LED_Destroy(void);
-LED_STATE LED_GetState(void); 
-void LED_On();
-void LED_Off();
 
 #endif // __LED_H__

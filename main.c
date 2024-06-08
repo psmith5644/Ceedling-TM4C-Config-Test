704dc274d86698888e5cb0098e9e2a8ca206dd8d
Patrick Smith <smithpm@usc.edu>
2024-06-07 17:31:56 -0700

    add correct initialization calls to main and delay

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include "LEDController.h"
+#include "LED.h"
+#include "SW1.h"
 
 int main(void) {
+    LED_Create();
+    SW1_Create();
     LEDController_Create();
     while (1) {
         LEDController_UpdateLED();
+        for (int i = 0; i < 1000000; i++) {}
     }
 }

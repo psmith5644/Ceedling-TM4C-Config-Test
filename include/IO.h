f3447621211d2f171b7c60dc9900306ae41b6303
Patrick Smith <smithpm@usc.edu>
2024-06-07 17:42:40 -0700

    Adding new functions to IO: Set and Clear bits.  This adds the ability to perform bitwise OR and AND operations instead of writing the whole register.  Adjust ioData type to be 4 bytes

--- include/IO.h
+++ include/IO.h
@@ -4,8 +4,10 @@
 #include <stdint.h>
 
 typedef uint32_t ioAddress;
-typedef uint16_t ioData;
+typedef uint32_t ioData;
 ioData IO_Read(ioAddress offset);
 void IO_Write(ioAddress offset, ioData data);
+void IO_SetBits(ioAddress offset, ioData data);
+void IO_ClearBits(ioAddress offset, ioData data);
 
 #endif // __IO_H__

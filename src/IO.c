0ed25f7365d6ffaf49eb5dc2dc307bacf3c41c29
Patrick Smith <smithpm@usc.edu>
2024-06-11 17:21:02 -0700

    Remove previous untested IO implementations

--- src/IO.c
+++ src/IO.c
@@ -27,23 +27,3 @@ void IO_ClearBits(ioAddress offset, ioData data) {
     uint32_t * ptr = (uint32_t *)offset;
     *ptr &= data;
 }
-
-// ioData IO_Read(ioAddress offset) {
-//     volatile uint32_t * addr = (volatile uint32_t *)offset;
-//     return (ioData)*addr;
-// }
-
-// void IO_Write(ioAddress offset, ioData data) {
-//     volatile uint32_t * addr = (volatile uint32_t *)offset;
-//     *addr = data;
-// }
-
-// void IO_SetBits(ioAddress offset, ioData data) {
-//     volatile uint32_t * addr = (volatile uint32_t *)offset;
-//     *addr |= data;
-// }
-
-// void IO_ClearBits(ioAddress offset, ioData data) {
-//     volatile uint32_t * addr = (volatile uint32_t *)offset;
-//     *addr &= data;
-// }

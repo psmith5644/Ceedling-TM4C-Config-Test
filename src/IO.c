bc8f2a6a4c2c299e1a7723e88a0870750f029284
Patrick Smith <smithpm@usc.edu>
2024-06-11 17:10:18 -0700

    test-driving IO_SetBits with testIOSetBitsZerosDoNotClear

--- src/IO.c
+++ src/IO.c
@@ -18,6 +18,11 @@ void IO_Write(ioAddress offset, ioData data) {
     *ptr = data;
 }
 
+void IO_SetBits(ioAddress offset, ioData data) {
+    uint32_t * ptr = (uint32_t *)offset;
+    *ptr |= data;
+}
+
 // ioData IO_Read(ioAddress offset) {
 //     volatile uint32_t * addr = (volatile uint32_t *)offset;
 //     return (ioData)*addr;
--- test/IO/test_IO.c
+++ test/IO/test_IO.c
@@ -26,3 +26,11 @@ void testIOWrite(void) {
 
     TEST_ASSERT_EQUAL_INT32(0xDEADBEEF, IO_Read((ioAddress)offset));
 }
+
+void testIOSetBitsZerosDoNotClear(void) {
+    offset = malloc(sizeof(uint32_t));
+    IO_Write((ioAddress)offset, 0xDEAD0000);
+    IO_SetBits((ioAddress)offset, 0x0000BEEF);
+
+    TEST_ASSERT_EQUAL_INT32(0xDEADBEEF, IO_Read((ioAddress)offset));
+}
